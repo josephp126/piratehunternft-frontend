66a06d08369e74c3e4ac11deadf5543b8b4ae2a3
Gary <garyluk7@gmail.com>
2021-12-12 23:06:12 -0500

    Update images.js

--- src/components/images.js
+++ src/components/images.js
@@ -6,9 +6,9 @@ const images = [
     {id: 5, src: require("../imgs/items/item5.png").default, description: "Bomb: -10% chance of tax when unstaking over 50k (1 time use)", price: 15000, qty: 2},
     {id: 6, src: require("../imgs/items/item6.png").default, description: "B-Rank: Collect Rank-Up to B-Rank Pirate (Prereq: Hold C-Rank Pirate)", price: 300000, qty: 2},
     {id: 7, src: require("../imgs/items/item7.png").default, description: "A-Rank: Collect Rank-Up to A-Rank Pirate (Prereq: Hold B-Rank Pirate)", price: 450000, qty: 2},
-    {id: 8, src: require("../imgs/items/rankc.png").default, description: "Bounty Quest Rank C: Collect Pirates with (2) specific properties     Reward: 20000 $BOOTY)", price: 5000, qty: 2},
-    {id: 9, src: require("../imgs/items/rankb.png").default, description: "Bounty Quest Rank B: Collect Pirates with (3) specific properties     Reward: 30000 $BOOTY)", price: 10000, qty: 2},
-    {id: 10, src: require("../imgs/items/ranka.png").default, description: "Bounty Quest Rank A: Collect Pirates with (4) specific properties      Reward: 50000 $BOOTY", price: 15000, qty: 2},
+    {id: 8, src: require("../imgs/items/rankc.png").default, description: "Bounty Quest Rank C: Collect Pirates with (2) specific properties     Reward: 60000 $BOOTY)", price: 5000, qty: 2},
+    {id: 9, src: require("../imgs/items/rankb.png").default, description: "Bounty Quest Rank B: Collect Pirates with (3) specific properties     Reward: 80000 $BOOTY)", price: 10000, qty: 2},
+    {id: 10, src: require("../imgs/items/ranka.png").default, description: "Bounty Quest Rank A: Collect Pirates with (4) specific properties      Reward: 100000 $BOOTY", price: 15000, qty: 2},
 ]
 
 export default images
