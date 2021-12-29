d298d3c6b5e1da11a1da2fc3ef1d1087430fc58c
Gary <garyluk7@gmail.com>
2021-12-28 19:00:05 -0500

    Delete Booty directory

--- Booty/IBooty.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.8.10;
-
-// SPDX-License-Identifier: MIT
-
-interface IBooty {
-  function mint(address to, uint256 amount) external;
-  function burn(address from, uint256 amount) external;
-}
